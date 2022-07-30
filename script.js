21d0571f39b420b1520d79146f6b31dc3a4bf7c2
Julian Longmire <julian@Julians-MacBook-Pro.local>
2022-07-29 21:43:03 -0700

    Added styling, other minor updates.

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 // ** VARIABLES ** //
 var button = document.querySelector("#button");
-var timer = 10;
+var timer = 120;
 var timerEl = document.querySelector('#timer');
+var question1 = ['Which of the following is your preferred type of pizza?', 'Cheese', 'Pepperoni', 'Meat Lovers', 'Vegetarian']
 
 
 
 // Let's Make 'QBOX'
 var main = document.querySelector("#main");
 var qboxEl = document.createElement('div');
-var qslotEl = document.createElement('h2');
+var qslotEl = document.createElement('h3');
 var olEl = document.createElement('ol');
 var li0El = document.createElement('li');
 var li1El = document.createElement('li');
 var li2El = document.createElement('li');
 var li3El = document.createElement('li');
-// A visual representation of Qbox 
-// qslotEl.textContent = `Hey there! what's your name?`;
-// li0El.textContent = 'Matthew';
-// li1El.textContent = 'Joseph';
-// li2El.textContent = 'Mary';
-// li3El.textContent = 'Kate';
+//A visual representation of Qbox 
+qslotEl.textContent = question1[0]
+li0El.textContent = question1[1]
+li1El.textContent = question1[2]
+li2El.textContent = question1[3]
+li3El.textContent = question1[4]
 
 
 
@@ -30,7 +31,7 @@ function starttimer() {
     setInterval(todo, 1000)
 }
 
-// Timer- what to do function
+// Timer- setinterval function
 function todo() {
     if (timer >= 0){
      console.log(timer);
@@ -41,10 +42,8 @@ function todo() {
         timerEl.appendChild(h2El);
 
      timer--;
-    }
+    } 
 }
-
-
 
 // Make QBOX 
 function makeqbox() {
@@ -60,16 +59,7 @@ function makeqbox() {
 
 
 
-
-
 // ** ACTION ** // 
 // On click start, start timer and add 'qbox' to main
 button.addEventListener('click', starttimer);
-
-
-
-
-
 button.addEventListener('click', makeqbox);
-
-
